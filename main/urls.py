9f4d4603c9d7f7449a2241cf2e9adfa1d863ea1f
TartejBrothers <taranjeetsinghbedi2@gmail.com>
2023-10-18 01:19:19 +0530

    new

--- main/urls.py
+++ main/urls.py
@@ -9,5 +9,3 @@ urlpatterns = [
     path("signup/", views.signup, name="signup"),
     path("details/", views.items, name="details"),
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
