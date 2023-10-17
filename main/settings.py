f44dd8c914fd9fc4916c463bcde380698b8bdc4d
TartejBrothers <taranjeetsinghbedi2@gmail.com>
2023-10-18 01:26:41 +0530

    new

--- main/settings.py
+++ main/settings.py
@@ -16,7 +16,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMP_DIR = os.path.join(BASE_DIR, "templates")
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "files/static"),)
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles_build", "static")
 
 
--- templates/static/script.js
+++
@@ -1,9 +0,0 @@
-e = document.getElementById("mobmenu");
-f = document.getElementById("menuopen");
-function menuopenclose() {
-  if (e.style.display == "block") {
-    e.style.display = "none";
-  } else {
-    e.style.display = "block";
-  }
-}
